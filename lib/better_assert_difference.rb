805daf2d2f5b05cfc7c02a26013cf8cc30096099
Julien Meichelbeck <julien.meichelbeck@doctolib.fr>
2017-01-04 13:50:36 +0100

    Improve error message

--- lib/better_assert_difference.rb
+++ lib/better_assert_difference.rb
@@ -31,11 +31,15 @@ module BetterAssertDifference
     errors = []
     before.zip(after, expression_to_diff) do |before_value, after_value, (exp, diff)|
       next if before_value + diff == after_value
-      error  = "#{exp.inspect} didn't change by #{diff} (before: #{before_value}, after: #{after_value})"
+      error = "[#{expression_to_diff.find_index { |expression, _| expression == exp }}] " if expression_to_diff.size > 1
+      error  = "#{error}#{exp.inspect} didn't change by #{diff} {before: #{before_value}, after: #{after_value}}"
       error  = "#{message}.\n#{error}" if message
       errors << error
     end
-    fail errors.join("\n") if errors.any?
+    if errors.any?
+      errors.unshift "#{errors.size} assertion#{errors.length > 1 ? 's' : ''} failed:"
+      fail errors.join("\n")
+    end
 
     retval
   end
